b10687e1d56efc62dd634656e149e2501bc26114
Charan Teja <96309627+Charantej07@users.noreply.github.com>
2025-02-02 23:27:59 +0530

    Frontend initialization, updated readme file

---
+++ front-end/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="container bg-red-600">
+        <h1>Hello Tailwind Css</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
