b5e8455834ab00af3745c388417eae1908aefbe4
Charan Teja <96309627+Charantej07@users.noreply.github.com>
2025-02-04 23:41:09 +0530

    Added Signup page, eye button for passwords

--- front-end/src/api/authApi.js
+++ front-end/src/api/authApi.js
@@ -10,3 +10,12 @@ export const loginUser = async (username, password) => {
     throw error.response?.data?.message || "Login failed";
   }
 };
+
+export const registerUser = async (email, password) => {
+  try {
+    const response = await axios.post(`${API_URL}/register`, { username: email, password });
+    return response.data;
+  } catch (error) {
+    throw error.response?.data?.message || "Signup failed";
+  }
+};
